d7b7dcd32429f5f180a6d6d9a47f0e7797acd5e0
Rteekasingh <rteekasingh@gmail.com>
2021-10-17 21:58:20 -0400

    Made some changes

--- Develop/index.js
+++ Develop/index.js
@@ -1,105 +1,148 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
-const util = require("util");
-const writeFileAsync = util.promisify(fs.writeFile);
+// Global require's
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./src/generateMarkdown.js')
 
-function promptUser() {
-    
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: "project_title",
-            message: "What is your project tittled?"
-          },
-          {
-            type: "input",
-            name: "description",
-            message: "Briefly describe your project"
-          },
-          {
-            type: "input",
-            name: "install",
-            message: "Are there any installations required?"
-          },
-          {
-            type: "input",
-            name: "use",
-            message: "What is the use of the application"
-          },
-          {
-            type: "input",
-            name: "contributions",
-            message: "Are there any contribution rules?"
-          },
-          {
-            type: "input",
-            name: "test",
-            message: "Please provide test instructions if applicable"
-          },
-          {
-            type: "checkbox",
-            message: "License?",
-            name: "license",
-            choices: [
-              "[MIT License](LICENSE.txt)", 
-              "[GNU GPLv3 License](COPYING.txt)", 
-            ]
-          },
-          {
-            type: "input",
-            name: "email",
-            message: "Enter your email account"
-          },
-          {
-            type: "input",
-            name: "github",
-            message: "Enter your github username"
-          }
-        ]);
-      }
+// Array of questions to ask the user
+const questions = [
+    // Project name
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of the project? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('You need to enter a title to continue!');
+                return false;
+            }
+        }
+    },
+    // Description of project
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('You need to provide a project description!');
+                return false;
+            }
+        }
+    },
+    // Installation Instructions
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install your project? (Required)',
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('You need to provide installation info to continue!');
+                return false;
+            }
+        }
+    },
+    // Usage Information
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this project? (Required)',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('You need to provide information on how to use project!');
+                return false;
+            }
+        }
+    },
+    // Contribution Guidlines
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How should people contribute to this project? (Required)',
+        validate: contributionInput => {
+            if (contributionInput) {
+                return true;
+            } else {
+                console.log('You need to provide information on how to contribute to the project!');
+                return false;
+            }
+        }
+    },
+    // Test Instructions 
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'How do you test this project? (Required)',
+        validate: testingInput => {
+            if (testingInput) {
+                return true;
+            } else {
+                console.log('You need to describe how to test this project!');
+                return false;
+            }
+        }
+    },
+    // License Options
+    {
+        type: 'checkbox',
+        name: 'licensing',
+        message: 'Choose a license for your project (Required)',
+        choices: ['Apache', 'MIT', 'Mozilla-Public', 'GNU-General-Public', 'Common-Development-and Distribution', 'None'],
+        validate: licensingInput => {
+            if (licensingInput) {
+                return true;
+            } else {
+                console.log('You must pick a license for the project!');
+                return false;
+            }
+        }
+    },
+    // Github Username
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username (Required)',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username!');
+                return false;
+            }
+        }
+    },
+    // Email Address
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Would you like to include your email?',
+    },
+];
 
-function generateREADME(answers) {
-  return `# ${answers.project_title}
-    
-#### Table of Contents
-1. [Project Description](#project-description)
-2. [Installation Instructions](#installation-instructions)
-3. [Usage Information](#usage-information)
-4. [Contributor Guidelines](#contributor-guidelines)
-5. [Code of Conduct](#code-of-conduct)
-6. [Test Instructions](#test-instructions)
-7. [License](#license)
-8. [Questions](#questions)
-## Project Description
-* ${answers.description}
-## Installation Instructions
-* ${answers.install}
-## Usage Information
-* ${answers.use}
-## Contributor Guidelines
-* ${answers.contributions}
-## Code of Conduct
-* [Contributor Covenant Code of Conduct](https://www.contributor-covenant.org/version/2/0/code_of_conduct/code_of_conduct.md)
-## Test Instructions
-* ${answers.test}
-## License
-* licensed under the ${answers.license}
-## Questions
-* For additional help or questions about collaboration, please reach out to ${answers.email}
-* Follow me on Github at [${answers.github}](http://github.com/${answers.github})`;
-  
-}
+// Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err)
+            throw err;
+        console.log('Success! Information transferred to the README!')
+    });
+};
 
-promptUser()
-  .then(function(answers) {
-    const readme = generateREADME(answers);
+// Function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then(function (userInput) {
+        console.log(userInput)
+        writeToFile("README.md", generateMarkdown(userInput));
+    });
+};
 
- 
-    return writeFileAsync("README.md", readme);
-  })
-  .then(function() {
-    console.log(" README.md has been created!");
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+// Function call to initialize app
+init();
